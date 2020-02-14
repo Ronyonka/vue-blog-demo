563450fef4cc31c3d42db5710d22ba415de3fe6a
ronyonka <rononyonka@gmail.com>
2020-02-14 12:11:19 +0300

    added blog nav component

---
+++ src/components/Blog.vue
@@ -0,0 +1,54 @@
+<template>
+    <main>
+         <blog-nav :content="content" :filters="filters" :navs="navs"/>
+    </main>
+</template>
+
+<script>
+import BlogNav from './BlogNav'
+export default {
+    name: 'blog',
+    components: { BlogNav },
+    resource: 'Blog',
+    props: {
+        post: String,
+        author: String
+    },
+
+    data() {
+        return {
+            navs: 0,
+            title: '',
+            labels: {
+                post: '',
+                author: ''
+            }
+        }
+    },
+
+    computed: {
+        content() {
+            return { title: this.title, labels: this.labels }
+        },
+        filters() {
+            let filters = {}
+
+            if (this.post) filters.post = this.post
+            if (this.author) filters.author = this.author
+
+            return filters
+        },
+        watch: {
+            '$route.name' (to, from) {
+            if (to !== from) this.navs++
+            }
+        },
+        mounted() {
+            this.$getResource('blog')
+            .then(x => {
+                // use pace hook to know when rendering is ready
+            })
+        }
+    }
+}
+</script>
